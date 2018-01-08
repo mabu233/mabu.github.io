929332bdb44c9304cba342f8e3463b97986f4927
xiao <1572771370@qq.com>
2018-01-08 16:24:08 +0800

    md

--- c/reverse.c
+++ c/reverse.c
@@ -1,4 +1,23 @@
 //
 // Created by xiao on 18-1-8.
 //
+#include <stdio.h>
+#include <string.h>
 
+char* reverse(char* str);
+
+int main(){
+    char str[12] = "hello,world";
+    printf("%s\n", reverse(str));
+}
+
+char* reverse(char* str){
+    int len = 0;
+    len = strlen(str);
+    for(int i = 0; i <= len/2; i++){
+        char ch = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = ch;
+    }
+    return str;
+}
